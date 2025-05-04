211ee156daf2c48eb17a183c3e5d6505435f1062
woiiz <irfanzafwan06@gmail.com>
2025-05-04 11:09:12 +0000

    Changed image formats from JPEG to PNG

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,9 @@
 import bg from './bg.jpg'
 
 import picture from './picture.png'
-
+import ardini from './ardini.png'
+import ardini2 from './ardini2.png'
+import ardini3 from './ardini3.png'
 import confetti from './confetti.gif'
 
 import CakeSVG from './svg/CakeSVG'
@@ -12,5 +14,8 @@ export {
     bg,
     picture,
     confetti,
+    ardini,
+    ardini2,
+    ardini3,
     CakeSVG
 }
